f769fe9464e7be21ded0bf666706b4beaa66cbf4
Shunto Mizushima <shuntomizushima@Shuntos-MacBook-Pro.local>
2021-05-26 21:00:44 +0900

    added a new data file

--- data_analysis/get_listing_data.py
+++ data_analysis/get_listing_data.py
@@ -1,5 +1,6 @@
 import sys, getopt
 import numpy as np
+import csv
 
 def usage():
     print("NAME: get_listing_data.py -- ")
@@ -40,19 +41,63 @@ def main():
     #listing_threads = all_listing_threads[all_listing_threads[:, 0] == anon_item_id]
     print(all_listings.shape)
     print(all_listing_threads.shape)
+
+    all_listings_tmp = readFromCsv("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp_2.csv")
+    print(all_listings_tmp)
+    listings_fieldnames = all_listings_tmp[0]
+    print(listings_fieldnames)
+    all_listing_threads_tmp = readFromCsv("../ebay_best_offer_bargaining_data/anon_bo_threads_tmp_2.csv")
+    listing_threads_fieldnames = all_listing_threads_tmp[0]
+    print(listing_threads_fieldnames)
+    listings = []
+    # creating a dummy numpy array to make it possible to cocatanate
+    #listing_threads = np.empty([1, all_listing_threads.shape[1]])
     listing_threads = []
-    for anon_item_id in all_listings[:, 0]:
+    for listing in all_listings:
+        anon_item_id = listing[0]
         listing_threads_tmp = all_listing_threads[all_listing_threads[:, 0] == anon_item_id]
         if listing_threads_tmp.size != 0:
             #print(listing_threads_tmp.shape)
             print("id: {}, the number of threads: {}".format(anon_item_id, listing_threads_tmp.shape))
-            listing_threads.append(listing_threads_tmp)
+            listings.append(listing)
+            for listing_thread in listing_threads_tmp:
+                listing_threads.append(listing_thread)
+
+            #listing_threads = np.concatenate((listing_threads, listing_threads_tmp), axis=1)
+            #listing_threads.append(listing_threads_tmp)
     
     #print(listings)
     #print(listing_threads)
     #print(listing_threads)
     #for listing in listing_threads:
     #    print(listing)
+    
+    # removing the dummy numpy array created in the beginning
+    #listing_threads = listing_threads[:,1:]
+    
+    writeToCsv("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp_3.csv", listings_fieldnames, listings)
+    writeToCsv("../ebay_best_offer_bargaining_data/anon_bo_threads_tmp_3.csv", listing_threads_fieldnames, listing_threads)
+
+def writeToCsv(filename, fieldnames, data_list):
+    with open(filename, mode='w') as data:
+        data_writer = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.DictWriter(data, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for entry in data_list:
+            data_writer.writerow(entry)
+
+def readFromCsv(filepath):
+    data = []
+    with open(filepath, mode='r') as data_set:
+        csv_reader = csv.DictReader(data_set)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            data.append(row)
+            line_count += 1
+    return data
         
     
 if __name__ == "__main__":
