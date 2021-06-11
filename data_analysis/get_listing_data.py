97c8ad63bc6e29a7a77a2fa93ef780ca66f33719
Shunto <shuntomizushima@s41.lab>
2021-06-11 09:38:16 +0900

    modified get_listing_data.py

--- data_analysis/get_listing_data.py
+++ data_analysis/get_listing_data.py
@@ -1,6 +1,9 @@
 import sys, getopt
 import numpy as np
 import csv
+import time
+
+start_time = time.time()
 
 def usage():
     print("NAME: get_listing_data.py -- ")
@@ -25,44 +28,63 @@ def main():
         if opt == "-h":
             usage()
 
+    print("test loading listing data completed, it took {:f} seconds".format(time.time() - start_time))
+    loading_listing_data_start_time = time.time()
     # loading listing data (anon_bo_lists_tmp.csv)
-    # all_listings = np.genfromtxt("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp.csv", delimiter=",")
-    all_listings = np.genfromtxt("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp_2.csv", delimiter=",")
+    all_listings = np.genfromtxt("../ebay_best_offer_bargaining_data/anon_bo_lists.csv", delimiter=",")
+    #all_listings = np.genfromtxt("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp.csv", delimiter=",")
+    print("loading listing data completed, it took {:f} seconds".format(time.time() - loading_listing_data_start_time))
+    print("total time: {:f}".format(time.time() - start_time))
+    #all_listings = np.genfromtxt("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp_2.csv", delimiter=",")
     
     # loading listing thread data (anon_bo_threads.csv)
+    all_listing_threads = np.genfromtxt("../ebay_best_offer_bargaining_data/anon_bo_threads.csv", delimiter=",")
     #all_listing_threads = np.genfromtxt("../ebay_best_offer_bargaining_data/anon_bo_threads_tmp.csv", delimiter=",")
+    loading_listing_threads_data_start_time = time.time()
+    print("loading listing threads data completed, it took {:f} seconds".format(time.time() - loading_listing_threads_data_start_time))
+    print("total time: {:f}".format(time.time() - start_time))
+    
     #all_listing_threads = np.genfromtxt("../ebay_best_offer_bargaining_data/anon_bo_threads_tmp_2.csv", delimiter=",")
     #print(all_listing_threads[0:10])
 
     # saving to listings the listings corresponding to the anon_item_id
-    #listings = all_listings[all_listings[:, 0] == anon_item_id]
-    
-    # saving to listing_threads the threads corresponding to the anon_item_id
-    #listing_threads = all_listing_threads[all_listing_threads[:, 0] == anon_item_id]
-    #print(all_listings.shape)
-    #print(all_listing_threads.shape)
+    searching_listing_threads_data_start_time = time.time()
+    listings = all_listings[all_listings[:, 0] == anon_item_id]
+    print("searching listing data for the specified item id completed, it took {:f} seconds".format(time.time() - searching_listing_threads_data_start_time))
+    print("total time: {:f}".format(time.time() - start_time))
 
-    all_listings = readFromCsv("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp_2.csv")
-    listings_fieldnames = all_listings[0]
-    print(listings_fieldnames)
-    all_listing_threads = readFromCsv("../ebay_best_offer_bargaining_data/anon_bo_threads_tmp_2.csv")
-    listing_threads_fieldnames = all_listing_threads[0]
-    print(listing_threads_fieldnames)
-    listings = []
+    # saving to listing_threads the threads corresponding to the anon_item_id
+    searching_listing_threads_data_start_time = time.time()
+    listing_threads = all_listing_threads[all_listing_threads[:, 0] == anon_item_id]
+    print("searching listing threads data for the specified item id completed, it took {:f} seconds".format(time.time() - searching_listing_threads_data_start_time))
+    print("total time: {:f}".format(time.time() - start_time))
+    
+    print(listings.shape)
+    print(listing_threads.shape)
+    print("total time: {:f}".format(time.time() - start_time))
+    
+
+    #all_listings = readFromCsv("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp_2.csv")
+    #listings_fieldnames = all_listings[0]
+    #print(listings_fieldnames)
+    #all_listing_threads = readFromCsv("../ebay_best_offer_bargaining_data/anon_bo_threads_tmp_2.csv")
+    #listing_threads_fieldnames = all_listing_threads[0]
+    #print(listing_threads_fieldnames)
+    #listings = []
     # creating a dummy numpy array to make it possible to cocatanate
     #listing_threads = np.empty([1, all_listing_threads.shape[1]])
-    listing_threads = []
-    for listing in all_listings:
-        listing_bool = False
-        anon_item_id = listing['anon_item_id']
+    #listing_threads = []
+    #for listing in all_listings:
+    #    listing_bool = False
+    #    anon_item_id = listing['anon_item_id']
         #listing_threads_tmp = all_listing_threads[all_listing_threads[:, 0] == anon_item_id]
-        for listing_thread in all_listing_threads:
-            if listing_thread['anon_item_id'] == anon_item_id:
-                listing_threads.append(listing_thread)
-                listing_bool= True
+    #    for listing_thread in all_listing_threads:
+    #        if listing_thread['anon_item_id'] == anon_item_id:
+    #            listing_threads.append(listing_thread)
+    #           listing_bool= True
 
-        if listing_bool:
-            listings.append(listing)
+    #    if listing_bool:
+    #        listings.append(listing)
         
         #if listing_threads_tmp.size != 0:
         #if len(listing_therads_tmp) != 0:
@@ -84,8 +106,8 @@ def main():
     # removing the dummy numpy array created in the beginning
     #listing_threads = listing_threads[:,1:]
     
-    writeToCsv("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp_3.csv", listings_fieldnames, listings)
-    writeToCsv("../ebay_best_offer_bargaining_data/anon_bo_threads_tmp_3.csv", listing_threads_fieldnames, listing_threads)
+    #writeToCsv("../ebay_best_offer_bargaining_data/anon_bo_lists_tmp_3.csv", listings_fieldnames, listinlgs)
+    #writeToCsv("../ebay_best_offer_bargaining_data/anon_bo_threads_tmp_3.csv", listing_threads_fieldnames, listing_threads)
 
 def writeToCsv(filename, fieldnames, data_list):
     with open(filename, mode='w') as data:
